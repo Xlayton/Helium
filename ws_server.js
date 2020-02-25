1c2f65c46939bba20f97d76aee09c1d0869aa241
Eyoung88 <47370000+Eyoung88@users.noreply.github.com>
2020-02-24 18:48:28 -0700

    Remove user from server and say they left

--- ws_server.js
+++ ws_server.js
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 require('dotenv').config();
 const PORT = process.env.WSPORT;
 
-//To Run server use "node ws_server.js"
+//To Run server use "node ws_server.js" but be sure to use the correct WSPORT on ur end
 //Create the websocket
 const wss = new WebSocket.Server({ port: PORT });
 
@@ -18,7 +18,13 @@ wss.on('connection', function connection(ws) {
       ws.send(message);
     });
   ws.on('close', function close() {
-    console.log("closed");
+    if(websocketList.includes(ws)) {
+      websocketList = websocketList.filter((cli) => cli !== ws)
+      websocketList.forEach(bye => {
+        bye.send("User Disconnected");
+      });
+      console.log(websocketList.length);
+    }
   });
   });
 });
