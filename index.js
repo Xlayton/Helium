d20dbfcaec224965d6d37616fc46c133f5e96942
code-witch <code-witch@users.noreply.github.com>
2020-02-28 19:31:12 -0700

    Remove duplicate line

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ app.use(express.static(path.join(__dirname+'/public')));
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
-
-app.use(express.static(path.join(__dirname + '/public')));
 app.use(expressSession({
     secret: process.env.SECRET,
     saveUninitialized: true,
