5ba0fba7619db5416ea38f4ff07d99b9d92213f6
code-witch <code-witch@users.noreply.github.com>
2020-02-28 19:31:28 -0700

    User model

---
+++ models/user.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(id, name, email, icon, password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.icon = icon;
+        this.password = password;
+        
+    }
+}
