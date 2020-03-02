5df27abb14130f026ce3b03707f779096cc2aa39
Xlayton <weightequalsmasstimesgravity@gmail.com>
2020-03-02 16:40:07 -0700

    Users connected to chat

--- public/js/connection.js
+++ public/js/connection.js
@@ -4,22 +4,23 @@ socket.onmessage = function(e) {
     document.getElementById("display").innerHTML += "<p>" + e.data + "</p>";
 };
 socket.onopen = function(e) {
-    socket.send("User Connected");
+    socket.send("Connected");
 };
 socket.onerror = function(error) {
-console.log('WebSocket Error: ' + error);
+    console.log('WebSocket Error: ' + error);
 };
+
 function sendMessage() {
     var input = document.getElementById("input");
-    if(input.value.replace(/\s/g, "")) {
+    if (input.value.replace(/\s/g, "")) {
         socket.send(input.value);
         input.value = "";
     }
 }
 document.getElementById("send").addEventListener("click", sendMessage);
 
-document.getElementById("input").addEventListener("keydown", function (e) {
-    if(e.keyCode === 13){
+document.getElementById("input").addEventListener("keydown", function(e) {
+    if (e.keyCode === 13) {
         sendMessage();
     }
 });
